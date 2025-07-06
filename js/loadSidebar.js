7ce63a3a29bcf42120df2b58ab7537cf3e4d71ee
SonYoungJun <dudwnsths1@naver.com>
2025-07-06 20:40:22 +0900

    fix(sidebar): 사이드바 로고 부분 클릭 시 페이지 이동
    - 살리미 로고 클릭시 home2.html페이지로 이동

--- js/loadSidebar.js
+++ js/loadSidebar.js
@@ -7,6 +7,15 @@ export function loadSidebar(path = "../pages/sidebar.html") {
       .then((res) => res.text())
       .then((html) => {
         sidebar.innerHTML = html;
+
+        // 로고 부분 클릭 시 home2.html로 이동하도록 이벤트 등록
+        const logoArea = sidebar.querySelector("#logo-area");
+        if (logoArea) {
+          logoArea.style.cursor = "pointer";
+          logoArea.addEventListener("click", () => {
+            window.location.href = "/pages/home2.html";
+          });
+        }
       })
       .catch((err) => {
         console.error("사이드바 로딩 실패:", err);
